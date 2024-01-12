06e14276a0d7d04d14992e4d2265f6212f1da005
TimMark <noierrdev@proton.me>
2024-01-12 07:37:34 -0800

    added new image

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 
 import FeatImage01 from '@/public/images/IMG_0438.jpg'
-import FeatImage02 from '@/public/images/IMG_0441.jpg'
+import FeatImage02 from '@/public/images/IMG_0442.jpg'
 import FeatImage03 from '@/public/images/IMG_0437.jpg'
 import FeatImage04 from '@/public/images/IMG_0434.jpg'
-
+import FeatImage05 from '@/public/images/IMG_0440.jpg'
 
 export default function Zigzag() {
   return (
@@ -131,7 +131,7 @@ Home is located in a great neighbor with a nice private backyard.</p>
               </div>
             </div>
 
-            {/* */}
+            {/* 4th */}
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
@@ -167,6 +167,44 @@ Home is located in a great neighbor with a nice private backyard.</p>
               </div>
             </div>
             {/* */}
+
+            {/**/}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05} width={540} height={405} alt="Features 03" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Higher but Comfoter and safer</div>
+                  <h3 className="h3 mb-3">🌟 2th Floor containing bedrooms 🌟</h3>
+                  <p className="text-xl text-gray-400 mb-4">There are totally 4 bedrooms and 2 bathrooms. Bedrooms are mainly on 2th floor. You will be satisfied with so cool and opening structure.</p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Natural Light</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Good Ventilation</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Big Windows but Noise Proof</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            {/**/}
+            
 
           </div>
 
