b3787df05f17b937bf264f9f9c1075dfa88bbcb5
TimMark <noierrdev@proton.me>
2024-01-12 01:33:54 -0800

    writing changed

--- components/features.tsx
+++ components/features.tsx
@@ -6,8 +6,8 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 className="h2 mb-4">The beautiful home surrounded by<br/> clean wild environment is waiting for a new resident.</h2>
+            <p className="text-xl text-gray-400">Rent For Cheap Price</p>
           </div>
 
           {/* Items */}
